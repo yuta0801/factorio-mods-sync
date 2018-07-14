09a79eac9d70c175f34a9f9706851662430c52ca
yuta0801 <yuta20030801@icloud.com>
2018-07-14 10:23:48 +0800

    Move code to display prompt to lib/prompts.js

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const chalk = require('chalk')
-const inquirer = require('inquirer')
-const autocomplete = require('inquirer-autocomplete-prompt')
 const logo = require('./lib/logo')
 const auth = require('./lib/auth')
 const mods = require('./lib/mods')
 const util = require('./lib/util')
 const path = require('./lib/path')
 const multiplayer = require('./lib/multiplayer')
-
-inquirer.registerPrompt('autocomplete', autocomplete)
+const prompts = require('./lib/prompts')
 
 console.log(chalk.yellow(logo))
 
@@ -23,16 +20,7 @@ console.log(chalk.yellow(logo))
     name: e.name, value: e.game_id,
   }))
 
-  const { id } = await inquirer.prompt({
-    type: 'autocomplete',
-    name: 'id',
-    message: 'Select a server to sync mods with',
-    source: async (answersSoFar, input) => {
-      if (!input) return choices
-      const regex = new RegExp(input, 'i')
-      return choices.filter(e => regex.test(e.name))
-    },
-  })
+  const { id } = await prompts.selectServer(choices)
 
   const game = await multiplayer.getGameDetails(id)
 
